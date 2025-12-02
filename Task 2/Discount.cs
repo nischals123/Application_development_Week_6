3eadacde867c806be25a630607261451fc10657f
Nischal <nischals054@gmail.com>
2025-12-02 11:28:07 +0545

    Task 2 completed (khalas)

---
+++ Task 2/Discount.cs
@@ -0,0 +1,13 @@
+﻿public class Discount
+{
+    public delegate int Calculate(int a, int b);
+
+    public int Add(int a, int b) => a + b;
+    public int Subtract(int a, int b) => a - b;
+
+    public delegate double DiscountStrategy(double price);
+
+    public double FestivalDiscount(double price) => price * 0.80;   // 20% off
+    public double SeasonalDiscount(double price) => price * 0.90;   // 10% off
+    public double NoDiscount(double price) => price;                // no discount
+}
---
+++ Task 2/Program.cs
@@ -0,0 +1,24 @@
+﻿
+class Program
+{
+    static void Main(string[] args)
+    {
+        Discount obj = new Discount();
+
+        Discount.Calculate calcAdd = obj.Add;
+        Discount.Calculate calcSub = obj.Subtract;
+
+        Console.WriteLine("Add: " + calcAdd(10, 5));
+        Console.WriteLine("Subtract: " + calcSub(10, 5));
+
+        Discount.DiscountStrategy fest = obj.FestivalDiscount;
+        Discount.DiscountStrategy seas = obj.SeasonalDiscount;
+        Discount.DiscountStrategy none = obj.NoDiscount;
+
+        double price = 1000;
+
+        Console.WriteLine("Festival Discount Price: " + fest(price));
+        Console.WriteLine("Seasonal Discount Price: " + seas(price));
+        Console.WriteLine("No Discount Price: " + none(price));
+    }
+}
